d89bfe629867bdd799e69bd8ba7c96d90ae54039
pariaspe <pedroariasperez96@gamil.com>
2018-02-28 18:36:22 +0100

    Version final

--- aleat.py
+++ aleat.py
@@ -8,10 +8,15 @@ ITT+IAA
 """
 
 import webapp
+import random
+
 
 class aleat(webapp.webApp):
     def process(self, parsedRequest):
-        return ("200 OK", "<html><body><h1>Hola</h1></body></html>")
+        num = random.randrange(999999999)
+        html_answer = "<html><body><h1>Hola. <a href='" + str(num)
+        html_answer += "'>Dame otra<a/></h1></body></html>"
+        return ("200 OK", html_answer)
 
 if __name__ == '__main__':
     miapp = aleat('0.0.0.0', 1234)
